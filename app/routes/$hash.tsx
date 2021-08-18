614f2b7d5772e59cb01a2db138501896c432d412
Chris Child <chris.child@gmail.com>
2021-08-18 21:04:18 +0100

    redirect

---
+++ app/routes/$hash.tsx
@@ -0,0 +1,25 @@
+import type { LoaderFunction } from 'remix';
+import { redirect } from 'remix';
+import Hashids from 'hashids';
+import Database from '../database';
+
+export let loader: LoaderFunction = async ({ params }) => {
+  const hash = params.hash;
+
+  const hasher = new Hashids(
+    process.env.HASH_SALT || 'devsalt',
+    parseInt(process.env.HASH_LENGTH || '5', 10)
+  );
+
+  let id = hasher.decode(hash);
+
+  const { data, error } = await Database.from('urls')
+    .select('redirect_to')
+    .eq('id', id);
+
+  return redirect(data && data[0]['redirect_to']);
+};
+
+export default function Index() {
+  return <></>;
+}
